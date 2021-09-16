e1305ab27df9d299536217bfa30f0e7cf4b9fc29
noname2048 <sungwook.csw@gmail.com>
2021-09-16 21:51:48 +0900

    feat: add onClick event to navbar

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import './App.css';
 
 import { GiHamburgerMenu } from 'react-icons/gi';
 import Navbar from './components/Navbar';
+import { useState } from 'react';
 
 function App() {
+  const [showNav, setShowNav] = useState(false);
+
   return <div className='App'>
-    <header><GiHamburgerMenu /></header>
-    <Navbar />
+    <header><GiHamburgerMenu onClick={() => setShowNav(!showNav)}/></header>
+    {showNav && <Navbar />}
   </div>;
 }
 
